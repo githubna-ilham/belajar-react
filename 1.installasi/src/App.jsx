23768a7d0c3c084bbd9215365baa4e7117dc3544
gitlabna-ilham <surelna.ilham@gmail.com>
2023-11-08 23:03:08 +0700

    state awal catatan belanja

--- 1.installasi/src/App.jsx
+++ 1.installasi/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Welcome to Median skill 🚀</h1>
+      <h1>Welcome to Median Skill 🚀</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
